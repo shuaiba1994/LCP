96bad812583aabb0ea9ab826fee169df62362ef5
assad041 <assad043@gmail.com>
2018-05-22 09:36:55 +0600

    Test Commit

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2,14 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
-	
+
 
 	/**
 	 * Index Page for this controller.
 	 *
 	 * Maps to the following URL
 	 * 		http://example.com/index.php/welcome
-	 *	- or -
+	 *	- or - test
 	 * 		http://example.com/index.php/welcome/index
 	 *	- or -
 	 * Since this controller is set as the default controller in
